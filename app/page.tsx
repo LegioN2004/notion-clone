1784ff0bf6224a6864ab1d20321cbf5553d6ef5c
legion2004 <milindabarua04@gmail.com>
2025-02-04 22:51:00 +0530

    new stuff

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="">
+      <h1>helooooooooo</h1>
+      <Button>Click me broooo</Button>
+
+    </main>
+  );
+}
